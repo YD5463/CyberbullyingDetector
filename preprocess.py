5c754d4ab00112b2982a338c820beca244f7e598
Yosef Danan <yosefdanan1234@gmail.com>
2021-10-31 16:25:04 +0200

    preprocess: stopword + spelling features

--- preprocess.py
+++ preprocess.py
@@ -3,10 +3,11 @@ import os
 import numpy as np
 import pandas as pd
 import string
-from spellchecker import SpellChecker
 from sklearn.feature_extraction.text import CountVectorizer
 from nltk.corpus import stopwords
 from nltk.stem import WordNetLemmatizer
+from enchant.checker import SpellChecker
+
 import re
 
 
@@ -20,25 +21,27 @@ def merge_datasets() -> pd.DataFrame:
     return df
 
 
-def add_punctuation_features(df: pd.DataFrame) -> pd.DataFrame:
-    for ch in string.punctuation:
+def add_punctuation_and_stopwords_features(df: pd.DataFrame) -> pd.DataFrame:
+    for ch in list(string.punctuation) + stopwords.words('english'):
         df[ch] = df['Text'].astype(str).map(lambda s: s.count(ch) / len(s))
     return df
 
 
+def add_count_misspell_feature(df: pd.DataFrame) -> pd.DataFrame:
+    def helper(data: str) -> float:
+        spell = SpellChecker("en_US", data)
+        counter = 0
+        for _ in spell:
+            counter += 1
+        return counter / len(data)
+    df["misspell_count"] = df["Text"].swifter.apply(helper)
+
+
 def process_row(row):
-    words = set(nltk.corpus.words.words())
-    spell = SpellChecker()
     lemmatizer = WordNetLemmatizer()
-    row = row.translate(str.maketrans('', '', string.punctuation))
-    row = re.sub('\w*\d\w*', ' ', row)  # remove words with numbers
-    row = re.sub('[‘’“”…]', ' ', row)
-    row = row.lower()
-    stop_words = stopwords.words('english')
-    row = " ".join(lemmatizer.lemmatize(w) for w in nltk.wordpunct_tokenize(row))  # spell checker
-    row = " ".join(spell.correction(w) for w in nltk.wordpunct_tokenize(row))  # spell checker
-    return " ".join(w for w in nltk.wordpunct_tokenize(row) if
-                    w in words and w not in stop_words)  # word exist in corpus and remove stop words
+    # row = row.translate(str.maketrans('', '', string.punctuation))
+    row = " ".join(lemmatizer.lemmatize(w) for w in nltk.wordpunct_tokenize(row))
+    return " ".join(w for w in nltk.wordpunct_tokenize(row))  # word exist in corpus and remove stop words
 
 
 def to_one_hot_rep(df: pd.DataFrame) -> pd.DataFrame:
@@ -63,14 +66,15 @@ def preprocess(train_part=0.7, use_cache=True) -> (np.ndarray, np.ndarray, np.nd
         df = pd.read_csv(cleaned_output_path)
     else:
         df = merge_datasets()
-        df = add_punctuation_features(df)
-        df["Text"] = df["Text"].apply(process_row)
-        df = to_one_hot_rep(df)
+        df = add_punctuation_and_stopwords_features(df)
+        df = add_count_misspell_feature(df)
+        # df["Text"] = df["Text"].apply(process_row)
+        # df = to_one_hot_rep(df)
         df.to_csv(cleaned_output_path)
-        label_name = "oh_label"
-    x = df.drop(label_name)
+    label_name = "oh_label"
+    x = df.drop(label_name, axis=1)
     y = df[label_name]
     num_rows = x.shape[0]
     mask_train = np.zeros(num_rows, dtype=bool)
-    mask_train[np.random.choice(num_rows,int(num_rows*train_part), replace=False)] = True
-    return x[mask_train, :], y[mask_train], x[~mask_train,:],y[~mask_train]
+    mask_train[np.random.choice(num_rows, int(num_rows * train_part), replace=False)] = True
+    return x[mask_train, :], y[mask_train], x[~mask_train, :], y[~mask_train]
